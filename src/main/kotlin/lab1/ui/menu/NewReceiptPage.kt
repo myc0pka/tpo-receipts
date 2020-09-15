3b5c54df540ae204335e30e1e32885bbba6d9135
cdu <cdu145@protonmail.com>
2020-09-15 20:06:31 +0300

    Use MenuPage#requestNotEmptyInput() in NewReceiptPage

--- src/main/kotlin/lab1/ui/menu/NewReceiptPage.kt
+++ src/main/kotlin/lab1/ui/menu/NewReceiptPage.kt
@@ -1,6 +1,7 @@
 package lab1.ui.menu
 
 import lab1.command.CreateReceiptCommand
+import kotlin.math.min
 
 private const val NAME_MAX_LENGTH = 20
 
@@ -9,19 +10,11 @@ class NewReceiptPage : MenuPage() {
     private val command = CreateReceiptCommand()
 
     override fun show(): Action {
-        while (true) {
-            printToUser("Введите название чека: ", endLine = false)
-            val userInput = getUserInput()
-            if (userInput.isNotEmpty()) {
-                if (userInput.length <= NAME_MAX_LENGTH) {
-                    command.name = userInput
-                } else {
-                    command.name = userInput.substring(0, NAME_MAX_LENGTH)
-                }
-                return Action.ShowPage(ReceiptPeoplePage(command))
-            } else {
-                printToUser("Название не должно быть пустым")
-            }
-        }
+        val nameInput = requestNotEmptyInput(
+            message = "Введите название чека: ",
+            emptyInputMessage = "Название не должно быть пустым"
+        )
+        command.name = nameInput.substring(0, min(nameInput.length, NAME_MAX_LENGTH))
+        return Action.ShowPage(ReceiptPeoplePage(command))
     }
 }
