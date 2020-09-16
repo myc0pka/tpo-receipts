4e4bb937516c91adb8eeb33e80b0e5b31621a56f
cdu <cdu145@protonmail.com>
2020-09-16 18:49:49 +0300

    Add cancel option to receipt consumptions page

--- src/main/kotlin/lab1/ui/menu/ReceiptConsumptionPage.kt
+++ src/main/kotlin/lab1/ui/menu/ReceiptConsumptionPage.kt
@@ -19,45 +19,57 @@ class ReceiptConsumptionPage(
 
         object End : Option(text = "Готово")
         class AddConsumption(val person: Person) : Option(text = person.name)
+        object Cancel : Option(text = "Вернуться в главное меню")
     }
 
     override fun handleOptionInput(option: Option): Action {
-        when (option) {
-            is Option.AddConsumption -> {
-                while (true) {
-                    val consumedAmountInput = requestNotEmptyInput(
-                        message = "Введите количество: ",
-                        emptyInputMessage = "Количество не должно быть пустым"
-                    )
-                    if (consumedAmountInput.matches(RegularExpressions.POSITIVE_INT)) {
-                        val consumedAmount = consumedAmountInput.toInt()
-                        if (consumedAmount <= availableAmount) {
-                            val consumption = Consumption(
-                                item = consumedItem,
-                                person = option.person,
-                                amount = consumedAmount
-                            )
-                            createReceiptCommand.addConsumption(consumption)
-                            return if (consumedAmount == availableAmount) {
-                                Action.ShowPage(ReceiptItemsPage(createReceiptCommand))
-                            } else {
-                                Action.ShowPage(
-                                    ReceiptConsumptionPage(
-                                        createReceiptCommand,
-                                        consumedItem,
-                                        availableAmount = availableAmount - consumedAmount
-                                    )
-                                )
-                            }
-                        } else {
-                            printToUser("Количество должно быть не больше остатка ($availableAmount)")
-                        }
-                    } else {
-                        printToUser("Количество должно быть целым положительным числом")
-                    }
-                }
-            }
-            Option.End -> return Action.ShowPage(ReceiptItemsPage(createReceiptCommand))
+        return when (option) {
+            is Option.AddConsumption -> addConsumption(option)
+            Option.End -> end()
+            Option.Cancel -> cancel()
         }
+    }
+
+    private fun addConsumption(option: Option.AddConsumption): Action {
+        while (true) {
+            val consumedAmountInput = requestNotEmptyInput(
+                message = "Введите количество: ",
+                emptyInputMessage = "Количество не должно быть пустым"
+            )
+            if (consumedAmountInput.matches(RegularExpressions.POSITIVE_INT)) {
+                val consumedAmount = consumedAmountInput.toInt()
+                if (consumedAmount <= availableAmount) {
+                    val consumption = Consumption(
+                        item = consumedItem,
+                        person = option.person,
+                        amount = consumedAmount
+                    )
+                    createReceiptCommand.addConsumption(consumption)
+                    return if (consumedAmount == availableAmount) {
+                        Action.ShowPage(ReceiptItemsPage(createReceiptCommand))
+                    } else {
+                        Action.ShowPage(
+                            ReceiptConsumptionPage(
+                                createReceiptCommand,
+                                consumedItem,
+                                availableAmount = availableAmount - consumedAmount
+                            )
+                        )
+                    }
+                } else {
+                    printToUser("Количество должно быть не больше остатка ($availableAmount)")
+                }
+            } else {
+                printToUser("Количество должно быть целым положительным числом")
+            }
+        }
+    }
+
+    private fun end(): Action {
+        return Action.ShowPage(ReceiptItemsPage(createReceiptCommand))
+    }
+
+    private fun cancel(): Action {
+        return Action.ShowPage(MainMenuPage())
     }
 }
