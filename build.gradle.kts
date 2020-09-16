fab32a07f87ace0524bc2849d770f4fb66ee5ae3
cdu <cdu145@protonmail.com>
2020-09-16 08:50:29 +0300

    Add Exposed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,15 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    val exposedVersion = "0.24.1"
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 }
