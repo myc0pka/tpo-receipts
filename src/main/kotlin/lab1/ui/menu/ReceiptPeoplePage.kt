f45b2e2b9ddb36220e805e333b8ce49a6d1099d2
cdu <cdu145@protonmail.com>
2020-09-16 18:40:37 +0300

    Add cancel option to receipt people page

--- src/main/kotlin/lab1/ui/menu/ReceiptPeoplePage.kt
+++ src/main/kotlin/lab1/ui/menu/ReceiptPeoplePage.kt
@@ -12,29 +12,39 @@ class ReceiptPeoplePage(private val createReceiptCommand: CreateReceiptCommand) 
     enum class Option(override val text: String) : MenuOption {
 
         ADD_PERSON(text = "Добавить человека"),
-        END(text = "Перейти к следующему шагу")
+        END(text = "Перейти к следующему шагу"),
+        CANCEL(text = "Вернуться в главное меню")
     }
 
     override fun handleOptionInput(option: Option): Action {
         return when (option) {
-            Option.ADD_PERSON -> {
-                val personNameInput = requestNotEmptyInput(
-                    message = "Введите имя человека: ",
-                    emptyInputMessage = "Имя не должно быть пустым"
-                )
-                val person = Person(name = personNameInput.substring(0, min(personNameInput.length, PERSON_NAME_MAX_LENGTH)))
-                createReceiptCommand.addPerson(person)
-
-                Action.ShowPage(this)
-            }
-            Option.END -> {
-                if (createReceiptCommand.personCount == 0) {
-                    printToUser("Добавьте по крайней мере одного человека")
-                    Action.ShowPage(this)
-                } else {
-                    Action.ShowPage(ReceiptItemsPage(createReceiptCommand))
-                }
-            }
+            Option.ADD_PERSON -> addPerson()
+            Option.END -> end()
+            Option.CANCEL -> cancel()
         }
+    }
+
+    private fun addPerson(): Action {
+        val personNameInput = requestNotEmptyInput(
+            message = "Введите имя человека: ",
+            emptyInputMessage = "Имя не должно быть пустым"
+        )
+        val person = Person(name = personNameInput.substring(0, min(personNameInput.length, PERSON_NAME_MAX_LENGTH)))
+        createReceiptCommand.addPerson(person)
+
+        return Action.ShowPage(this)
+    }
+
+    private fun end(): Action {
+        return if (createReceiptCommand.personCount == 0) {
+            printToUser("Добавьте по крайней мере одного человека")
+            Action.ShowPage(this)
+        } else {
+            Action.ShowPage(ReceiptItemsPage(createReceiptCommand))
+        }
+    }
+
+    private fun cancel(): Action {
+        return Action.ShowPage(MainMenuPage())
     }
 }
