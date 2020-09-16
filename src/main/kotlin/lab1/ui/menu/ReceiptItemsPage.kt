91685b14e2729c68832a3185f991baad80b62a6a
cdu <cdu145@protonmail.com>
2020-09-16 18:44:26 +0300

    Add cancel option to receipt items page

--- src/main/kotlin/lab1/ui/menu/ReceiptItemsPage.kt
+++ src/main/kotlin/lab1/ui/menu/ReceiptItemsPage.kt
@@ -13,68 +13,78 @@ class ReceiptItemsPage(private val createReceiptCommand: CreateReceiptCommand) :
     enum class Option(override val text: String) : MenuOption {
 
         ADD_ITEM(text = "Добавить товар"),
-        END(text = "Завершить")
+        END(text = "Завершить"),
+        CANCEL(text = "Вернуться в главное меню")
     }
 
     override fun handleOptionInput(option: Option): Action {
         return when (option) {
-            Option.ADD_ITEM -> {
-                val nameInput = requestNotEmptyInput(
-                    message = "Введите название товара: ",
-                    emptyInputMessage = "Название не должно быть пустым"
-                )
-                val amount: Int
-                while (true) {
-                    val amountInput = requestNotEmptyInput(
-                        message = "Введите количество: ",
-                        emptyInputMessage = "Количество не должно быть пустым"
-                    )
-                    if (amountInput.matches(RegularExpressions.POSITIVE_INT)) {
-                        amount = amountInput.toInt()
-                        break
-                    } else {
-                        printToUser("Количество должно быть целым положительным числом")
-                    }
-                }
-                val price: Double
-                while (true) {
-                    val priceInput = requestNotEmptyInput(
-                        message = "Введите цену: ",
-                        emptyInputMessage = "Цена не должна быть пустой"
-                    )
-                    if (priceInput.matches(RegularExpressions.POSITIVE_REAL)) {
-                        price = priceInput.toDouble()
-                        break
-                    } else {
-                        printToUser("Цена должна быть целым вещественным числом")
-                    }
-                }
+            Option.ADD_ITEM -> addItem()
+            Option.END -> end()
+            Option.CANCEL -> cancel()
+        }
+    }
 
-                val item = ReceiptItem(
-                    name = nameInput.substring(0, min(nameInput.length, RECEIPT_ITEM_NAME_MAX_LENGTH)),
-                    amount, price
-                )
-                createReceiptCommand.addItem(item)
-                Action.ShowPage(
-                    ReceiptConsumptionPage(
-                        createReceiptCommand,
-                        consumedItem = item,
-                        availableAmount = item.amount
-                    )
-                )
-            }
-            Option.END -> {
-                if (createReceiptCommand.itemCount == 0) {
-                    printToUser("Добавьте по крайней мере один товар")
-                    Action.ShowPage(this)
-                } else {
-                    printToUser("Создание чека...", endLine = false)
-                    if (createReceiptCommand.execute() == CreateReceiptCommand.Result.Ok) {
-                        printToUser("Готово")
-                    }
-                    Action.ShowPage(MainMenuPage())
-                }
+    private fun addItem(): Action {
+        val nameInput = requestNotEmptyInput(
+            message = "Введите название товара: ",
+            emptyInputMessage = "Название не должно быть пустым"
+        )
+        val amount: Int
+        while (true) {
+            val amountInput = requestNotEmptyInput(
+                message = "Введите количество: ",
+                emptyInputMessage = "Количество не должно быть пустым"
+            )
+            if (amountInput.matches(RegularExpressions.POSITIVE_INT)) {
+                amount = amountInput.toInt()
+                break
+            } else {
+                printToUser("Количество должно быть целым положительным числом")
             }
         }
+        val price: Double
+        while (true) {
+            val priceInput = requestNotEmptyInput(
+                message = "Введите цену: ",
+                emptyInputMessage = "Цена не должна быть пустой"
+            )
+            if (priceInput.matches(RegularExpressions.POSITIVE_REAL)) {
+                price = priceInput.toDouble()
+                break
+            } else {
+                printToUser("Цена должна быть целым вещественным числом")
+            }
+        }
+
+        val item = ReceiptItem(
+            name = nameInput.substring(0, min(nameInput.length, RECEIPT_ITEM_NAME_MAX_LENGTH)),
+            amount, price
+        )
+        createReceiptCommand.addItem(item)
+        return Action.ShowPage(
+            ReceiptConsumptionPage(
+                createReceiptCommand,
+                consumedItem = item,
+                availableAmount = item.amount
+            )
+        )
+    }
+
+    private fun end(): Action {
+        return if (createReceiptCommand.itemCount == 0) {
+            printToUser("Добавьте по крайней мере один товар")
+            Action.ShowPage(this)
+        } else {
+            printToUser("Создание чека...", endLine = false)
+            if (createReceiptCommand.execute() == CreateReceiptCommand.Result.Ok) {
+                printToUser("Готово")
+            }
+            Action.ShowPage(MainMenuPage())
+        }
+    }
+
+    private fun cancel(): Action {
+        return Action.ShowPage(MainMenuPage())
     }
 }
